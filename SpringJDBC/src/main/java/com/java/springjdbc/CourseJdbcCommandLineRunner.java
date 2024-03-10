a5fbf7293621fc11b10aeedf00df45a06d478754
Suchith-Balne <sb2648@njit.edu>
2024-03-09 22:48:07 -0500

    Added code to execute insert and delete queries

--- SpringJDBC/src/main/java/com/java/springjdbc/CourseJdbcCommandLineRunner.java
+++ SpringJDBC/src/main/java/com/java/springjdbc/CourseJdbcCommandLineRunner.java
@@ -1,5 +1,6 @@
 package com.java.springjdbc;
 
+import com.java.springjdbc.Course.Course;
 import com.java.springjdbc.Repository.CourseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -12,6 +13,12 @@ public class CourseJdbcCommandLineRunner implements CommandLineRunner {
     CourseRepository courseRepository;
     @Override
     public void run(String... args) throws Exception {
-        courseRepository.insert();
+        courseRepository.insert(new Course(1, "Java", "Suchith"));
+        courseRepository.insert(new Course(2, "C++", "Sameeri"));
+        courseRepository.insert(new Course(3, ".NET", "Balne"));
+
+        courseRepository.deleteById(2);
+
+        System.out.println(courseRepository.findById(1));
     }
 }
--- SpringJDBC/src/main/java/com/java/springjdbc/Repository/CourseRepository.java
+++ SpringJDBC/src/main/java/com/java/springjdbc/Repository/CourseRepository.java
@@ -1,23 +1,43 @@
 package com.java.springjdbc.Repository;
 
+import com.java.springjdbc.Course.Course;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class CourseRepository  {
 
+    @Autowired
     private JdbcTemplate jdbcTemplate;
 
     @Autowired
     private static String INSERT_QUERY =
             """
-            insert into Course values(1, 'Learn AWS', Suchith);
+            insert into Course values(?, ?, ?);
             """;
 
-    public void insert(){
-        jdbcTemplate.update(INSERT_QUERY);
+    private static  String DELETE_QUERY = """
+            delete from Course where id =?;
+            """;
+
+    private static  String SELECT_QUERY_BY_ID = """
+            select * from Course where id =?;
+            """;
+
+    public void insert(Course course){
+        jdbcTemplate.update(INSERT_QUERY, course.getId(), course.getName(), course.getAuthor());
     }
 
+    public void deleteById(long id){
+        jdbcTemplate.update(DELETE_QUERY, id);
+    }
+
+    public Course findById(long id){
+        return jdbcTemplate.queryForObject(SELECT_QUERY_BY_ID,
+                new BeanPropertyRowMapper<>(Course.class),id);
+
+    }
 
 }
